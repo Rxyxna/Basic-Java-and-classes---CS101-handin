85a5ce97dabbc85462a5bd2c402db968a05f1d33
Rxyxna <Rayanabaga@outlook.com>
2023-02-01 21:35:19 +0100

    Methods 1 - complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Methods 1
+        Scanner vote = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int age = Integer.parseInt(vote.nextLine());
+
+
+        if((age) >= 18) {
+            System.out.println("You are eligible to vote");
+        } if ((age) < 18){
+            System.out.println("You are not eligible to vote");
+        }
+
+        // Methods 2
+
+
     }
 }
